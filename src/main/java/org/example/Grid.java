23ec9c0e04c5fec1b503b061605d24a65e096a9b
Samrat <smaratrm2@gmail.com>
2023-05-09 23:06:31 +0530

    grid methods updated

--- src/main/java/org/example/Grid.java
+++ src/main/java/org/example/Grid.java
@@ -11,17 +11,22 @@ public class Grid {
         this.grid = createGrid(rows, columns);
     }
 
+    private Cell createCell(int row, int col) {
+        Address address = new Address(row, col);
+        boolean state = Math.random() < 0.5;
+        return new Cell(address, state);
+    }
+
     private Cell[][] createGrid(int rows, int columns) {
         Cell[][] grid = new Cell[rows][columns];
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < columns; col++) {
-                Address address = new Address(row, col);
-                boolean state = Math.random() < 0.5;
-                grid[row][col] = new Cell(address, state);
+                grid[row][col] = createCell(row, col);
             }
         }
         return grid;
     }
+
     public Cell getCell(int row, int col) {
         if (row >= 0 && row < rows && col >= 0 && col < columns) {
             return grid[row][col];
